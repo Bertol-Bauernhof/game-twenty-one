2bee2f1fdfa323a8084c0d0c90f526cb17842382
Bertolt <tweresty@outlook.com>
2022-06-17 22:50:48 +0300

    Add application

---
+++ application/Developer.cpp
@@ -0,0 +1,8 @@
+// Created by Bertolt Winterhalter on 5/07/2022.
+class Developer {
+    public:
+        static const std::string& NAME() {
+            static const std::string NAME = "Bertolt Winterhalter";
+        return NAME;
+        }
+};
---
+++ application/Player.cpp
@@ -0,0 +1,46 @@
+class Player {
+    private:
+        int count;
+        string faceCard;
+        string name;
+        int numberOfCards;
+
+    public:
+        // Setters
+        void setName(string name) {
+            this->name = name;
+        }
+        void setCount(int count) {
+            this->count = count;
+        }
+        void setNumberOfCards() {
+            this->numberOfCards += 1;
+        }
+        void setFaceCard(string faceCard) {
+            this->faceCard += faceCard + "|";
+        }
+
+        // Getters
+        int getCount() {
+            return count;
+        }
+        string getName() {
+            return name;
+        }
+        string getFaceCard() {
+            return faceCard;
+        }
+        int getNumberOfCards() {
+            return numberOfCards;
+        }
+
+        // Constructors
+        Player() {
+            this->name = "no name";
+            this->count = 0;
+        }
+        Player(string name) {
+            new (this) Player();
+            this->name = name;
+        }
+};
