34398f2af934c33ed3f39f905ccfafbdeda9045d
Bertolt <78558694+SpaceConer@users.noreply.github.com>
2022-06-27 12:39:31 +0300

    Delete application directory

--- application/Developer.cpp
+++
@@ -1,8 +0,0 @@
-// Created by Bertolt Winterhalter on 5/07/2022.
-class Developer {
-    public:
-        static const std::string& NAME() {
-            static const std::string NAME = "Bertolt Winterhalter";
-        return NAME;
-        }
-};
